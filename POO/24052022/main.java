d06d4246f815d810d7ae2d478ed3e844e9d9cc30
Marcos Godinho <104370082+MarcosGodinho@users.noreply.github.com>
2022-05-24 13:49:34 -0300

    Create main.java

---
+++ POO/24052022/main.java
@@ -0,0 +1,28 @@
+public class main {
+    public static void main(String[] args) {
+
+        RoboLunar robo = new RoboLunar();
+        
+        System.out.println(robo);
+
+        System.out.println(robo.moverParaFrente(10));
+
+        System.out.println(robo.virarRobo(90));
+
+        System.out.println(robo.moverParaTras(10));
+
+        System.out.println(robo.capturarFoto());
+        
+        System.out.println(robo);
+        
+        System.out.println("Carregando = " + robo.isCarregando());
+
+        try {
+            robo.carregarBateria();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+}
